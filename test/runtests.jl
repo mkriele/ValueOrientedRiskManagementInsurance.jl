f94155254f664779f422beda710d18c5d746ec9f
Marcus Kriele <mkriele@me.com>
2015-01-03 17:25:27 -0500

    First test (correction)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,25 @@
 using ValueOrientedRiskManagementInsurance
 using Base.Test
+using DataFrames
 
+t_0 = 0
+T = 5
 ## pricing assumptions
-rfr_price = rep(0.005, T)                         ## discount rate for pricing
+rfr_price = fill(0.005, T)                         ## discount rate for pricing
 prob_price = DataFrame( ## biometric probabilities
                        qx = 0.001 .+ [0:(T-1)] * 0.0001,
-                       sx = rep(0.1, T)
+                       sx = fill(0.1, T)
                        )
 λ_price =  DataFrame(  ## cost profile
                      boy = [0.05, rep(0.0, T-1)], ## costs beginning of year
-                     eoy = rep(0.04, T),          ## costs end of year
-                     infl = rep(0.01, T)          ## cost inflation per year
+                     eoy = fill(0.04, T),         ## costs end of year
+                     infl = fill(0.01, T)         ## cost inflation per year
                      )
 β = DataFrame( ## profile insurance contract
-              qx = rep(1.0, T),                   ## death benefit rel to insured sum
-              sx = cumsum(rep(0.9, T)),           ## lapse ben rel to premium
-              px = [rep(0.0, T-1), 1],            ## life ben  rel to insured sum
-              prem = rep(1.0, T)                  ## normalized premium payments
+              qx = fill(1.0, T),                  ## death benefit rel to insured sum
+              sx = cumsum(fill(0.9, T)),          ## lapse ben rel to premium
+              px = [fill(0.0, T-1), 1],           ## life ben  rel to insured sum
+              prem = fill(1.0, T)                 ## normalized premium payments
               )
 
 product = Product(rfr_price, prob_price, β, λ_price)
