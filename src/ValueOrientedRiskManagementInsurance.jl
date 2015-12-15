f7d17f436dc0b4663c90bf97a0f2128bd27f82c0
Marcus Kriele <mkriele@me.com>
2015-12-15 18:46:58 +0100

    Formatting

--- src/ValueOrientedRiskManagementInsurance.jl
+++ src/ValueOrientedRiskManagementInsurance.jl
@@ -1,18 +1,43 @@
 """
-The module provides example calculations for the second edition of the book
+The module provides example calculations for the second edition
+of the book
 
-> Kriele M. and Wolf, J. _Wertorientiertes Risikomanagement von  Versicherungsunternehmen_, 2nd edition, Springer-Verlag, Berlin Heidelberg,  2016 (to be published)
+> Kriele M. and Wolf, J.
+> _Wertorientiertes Risikomanagement von Versicherungsunternehmen_,
+>  2nd edition, Springer-Verlag, Berlin Heidelberg,  2016
+> (to be published)
 
-It is also intended to use these examples for future editions of the English  translation of this book,  _Value-Oriented Risk Management of Insurance Companies_.  (The examples in the current English edition are written in R).
+It is also intended to use these examples for future editions of
+the English  translation of this book,
+_Value-Oriented Risk Management of Insurance Companies_.
+(The examples in the current English edition are written in R).
 
- The module consists of 4 distinct parts:
+The module consists of 4 distinct parts:
 
- - *SSTLife*: An extremely simplified example of the SST (Swiss Solvency Test) calculation for life insurance. The Swiss Solvency Test is the Swiss regulatory capital requirement.  The resulting monetary requirement is referred to as the "target capital" `ZK`<sup>1</sup>.
- - *S2Life*: A simplified example of the S2 (Solvency 2) calculation for non-life insurance. Solvency 2 is the new regulatory capital requirement in the European Union. The resulting monetary requirement is referred to as the "Solvency capital requirement" `SCR`.
- - *S2NonLife*: A simplified example of the S2 calculation for life insurance
- - *ECModel*: An extremely simplified example of an internal economic capital model for non-life insurance. This model is used to illustrate some techniques used in value based management.
+- *SSTLife*: An extremely simplified example of the SST
+(Swiss Solvency Test) calculation for life insurance.
+The Swiss Solvency Test is the Swiss regulatory capital
+requirement.  The resulting monetary requirement is referred
+to as the "target capital" `ZK`.
+- *S2Life*: A simplified example of the S2 (Solvency 2)
+calculation for non-life insurance. Solvency 2 is the new
+regulatory capital requirement in the European Union. The
+resulting monetary requirement is referred to as the
+"Solvency capital requirement" `SCR`.
+- *S2NonLife*: A simplified example of the S2 calculation
+for life insurance
+- *ECModel*: An extremely simplified example of an internal
+economic capital model for non-life insurance. This model is
+used to illustrate some techniques used in value based
+management.
 
-Note that we have simplified and (in part changed for our exposition) the regulatory requirements for SST and Solvency 2. Also note that the implementation of Solvency 2 may be slightly different in different EU countries. For definitive information about SST or Solvency 2, please consult the original literature and any guidance issued by the supervisory authorities in the jurisdiction of interest.
+Note that we have simplified and (in part changed for our
+exposition) the regulatory requirements for SST and Solvency 2.
+Also note that the implementation of Solvency 2 may be slightly
+different in different EU countries. For definitive information
+about SST or Solvency 2, please consult the original literature
+and any guidance issued by the supervisory authorities in the
+jurisdiction of interest.
 """
 module ValueOrientedRiskManagementInsurance
 
