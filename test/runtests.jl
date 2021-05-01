f71660795d8d21cf99758ea719e3802f2c8f23ba
reumle <jpojpoj@gmail.com>
2021-05-01 16:49:39 +0200

    * corriger tous les dépréciation warnings (restants) du jeu de test.
    il reste un warning de collision de nom sur Distributions.Product , dans le test de SST.
    
    src\ECModel\ECModel_Functions.jl
    remplacer df[:myvar] par df[!,:myvar]
    
    src\S2NonLife\S2NonLife_Functions.jl
    remplacer df[:myvar] par df[!,:myvar]
    
    test\ECModel.jl
    idem
    
    test\ECModel_Input.jl
    idem
    
    test\S2NonLife.jl
    idem
    
    test\runtests.jl
    idem

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,12 @@ import LinearAlgebra.⋅
 
 println("Testing S2 Life")
 include("S2Life_Test.jl")
-# println("Testing SST Life")
-# include("SSTLife_Test.jl")
-# println("Testing S2 Non-Life")
-# include("S2NonLife_Test.jl")
-# println("Testing ECModel")
-# include("ECModel_Test.jl")
+println("Testing SST Life")
+include("SSTLife_Test.jl")
+println("Testing S2 Non-Life")
+include("S2NonLife_Test.jl")
+println("Testing ECModel")
+include("ECModel_Test.jl")
 
 
 println("tests completed")
